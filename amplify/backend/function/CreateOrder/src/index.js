041b2c4fe58941ef24ad53230ee9a3940bd2d474
chilubagh <chilubagh@gmail.coms>
2020-12-24 22:25:36 +0200

    aws credentials updated from the console

--- amplify/backend/function/CreateOrder/src/index.js
+++ amplify/backend/function/CreateOrder/src/index.js
@@ -1,15 +1,53 @@
+const { v4: uuidv4 } = require("uuid");
+const AWS = require("aws-sdk");
+const documentClient = new AWS.DynamoDB.DocumentClient();
 
+const ORDER_TABLE = "Order-274doapqr5f7rmuqbdfpgvfnwi-pro
+";
+const ORDER_TYPE = "Order";
+const BOOK_ORDER_TABLE = "BookOrder-274doapqr5f7rmuqbdfpgvfnwi-pro";
+const BOOK_ORDER_TYPE = "BookOrder";
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+const createOrder = async(payload) => {
+    const { order_id, username, email, total } = payload;
+    var params = {
+        TableName: ORDER_TABLE,
+        Item: {
+            id: order_id,
+            __typename: ORDER_TYPE,
+            customer: email,
+            user: username,
+            total: total,
+            updatedAt: new Date().toISOString(),
+            createdAt: new Date().toISOString()
+        }
     };
-    return response;
+    console.log(params);
+    await documentClient.put(params).promise();
+};
+
+const createBookOrder = async(payload) => {
+    let bookOrders = [];
+    for (i = 0; i < payload.cart.length; i++) {
+        const cartItem = payload.cart[i];
+        bookOrders.push({
+            PutRequest: {
+                Item: {
+                    id: uuidv4(),
+                    __typename: BOOK_ORDER_TYPE,
+                    book_id: cartItem.id,
+                    order_id: payload.order_id,
+                    customer: payload.email,
+                    createdAt: new Date().toISOString(),
+                    updatedAt: new Date().toISOString()
+                }
+            }
+        });
+    }
+    let params = {
+        RequestItems: {}
+    };
+    params["RequestItems"][BOOK_ORDER_TABLE] = bookOrders;
+    console.log(params);
+    await documentClient.batchWrite(params).promise();
 };
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,19 +5,24 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('<stripe_public_key>');
+    const stripePromise = loadStripe('pk_test_51I1z5iD6YOEbtS1b4bzSEVVvNwTDdvoUPskV2IcW6OAGFjVE4mIvm9t5mQxtjqMFtYeBXnJ5dOgUBQQJ7yMGJ9S2000SzHgMJBsk_test_51I1z5iD6YOEbtS1b23LvANRN33I1FIA2i0dtJzwJYyUgtafZoAcoKthj4OmFmH8jk02dOiJ32O0KgjQnSsyMwsMi00QOzNoAGB');
 
-    return (
-        <section className="checkout-wrapper">
-            <AmplifyAuthenticator>
-                <Elements stripe={stripePromise}>
-                    <section>
-                        <h2>Time to Checkout?</h2>
-                        <CheckoutForm />
-                    </section>
-                </Elements>
-            </AmplifyAuthenticator>
-        </section>
+    return ( <
+        section className = "checkout-wrapper" >
+        <
+        AmplifyAuthenticator >
+        <
+        Elements stripe = { stripePromise } >
+        <
+        section >
+        <
+        h2 > Time to Checkout ? < /h2> <
+        CheckoutForm / >
+        <
+        /section> < /
+        Elements > <
+        /AmplifyAuthenticator> < /
+        section >
     )
 }
 
